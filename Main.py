3de77472080a4fccc7e7209c342430194a86d19d
lkri <lkri@techcollege.dk>
2020-06-25 12:53:48 +0200

    added comments to first model and tuned the attributes to get between 75% - 91% accuracy.

--- Main.py
+++ Main.py
@@ -6,26 +6,37 @@ import sklearn
 from sklearn import linear_model
 from sklearn.utils import shuffle
 
+#diffing dataset and seperators
 data = pd.read_csv("student-mat.csv", sep=";")
-data = data[["G1", "G2", "G3", "failures", "studytime", "absences", "health"]]
+#filtering dataset
+data = data[["G1", "G2", "G3", "studytime"]]
 
+#Which attribute to be predicted
 predict = "G3"
 
+#Drops the value in predict from dataset
 X = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
 
+#Splits dataset up into trainig data and test data, so it does not train on the same data as it tests on.
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
 
+#makes a liniar regression model
 linear = linear_model.LinearRegression()
-
+#trains model
 linear.fit(x_train, y_train)
+#gets the accuracy of the model
 acc = linear.score(x_test, y_test)
-print(acc)
+print("Accuracy: \n", acc)
 
+#liniar regrssion function: Y = m + n + ... + z * X + b
+#gets the coefficient and the intercept values
 print('Coefficient: \n', linear.coef_)
 print('Intercept: \n', linear.intercept_)
 
+#the model makes predictions for the dataset with liniar regression
 predictions = linear.predict(x_test)
 
+#prints the predictions
 for x in range(len(predictions)):
     print(predictions[x], x_test[x], y_test[x])
