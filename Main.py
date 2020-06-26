475307b84333be7e96a4f69bc9ec842018e9a5c9
lkri <lkri@techcollege.dk>
2020-06-26 12:54:30 +0200

    Visualizing data relationship with graphs and feature find the best trained model and save it to a pickle file.

--- Main.py
+++ Main.py
@@ -4,28 +4,54 @@ import pandas as pd
 import numpy as np
 import sklearn
 from sklearn import linear_model
-from sklearn.utils import shuffle
+import matplotlib.pyplot as pyplot
+from matplotlib import style
+import pickle
 
 #diffing dataset and seperators
 data = pd.read_csv("student-mat.csv", sep=";")
 #filtering dataset
-data = data[["G1", "G2", "G3", "studytime"]]
+data = data[["G1", "G2", "G3", "studytime", "absences"]]
 
 #Which attribute to be predicted
 predict = "G3"
 
 #Drops the value in predict from dataset
-X = np.array(data.drop([predict], 1))
+x = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
-
 #Splits dataset up into trainig data and test data, so it does not train on the same data as it tests on.
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 
-#makes a liniar regression model
-linear = linear_model.LinearRegression()
-#trains model
-linear.fit(x_train, y_train)
-#gets the accuracy of the model
+"""
+#for loob code is for retraining a new model and saving it to a pickle file if accuracy is better
+bestAcc = 0
+for _ in range(500):
+    #Splits dataset up into trainig data and test data, so it does not train on the same data as it tests on.
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+
+    # makes a liniar regression model
+    linear = linear_model.LinearRegression()
+    # trains model
+    linear.fit(x_train, y_train)
+
+    # gets the accuracy of the model
+    acc = linear.score(x_test, y_test)
+    print("Accuracy: \n", acc)
+
+    if acc > bestAcc:
+        bestAcc = acc
+
+        # saves the model in a pickel file
+        with open("studentmodel.pickle", "wb") as f:
+            pickle.dump(linear, f)
+
+print("Best Accuracy: \n", bestAcc)"""
+
+#loads the trained model from a pickle file
+pickle_in = open("studentmodel.pickle", "rb")
+linear = pickle.load(pickle_in)
+
+# gets the accuracy of the model
 acc = linear.score(x_test, y_test)
 print("Accuracy: \n", acc)
 
@@ -40,3 +66,10 @@ predictions = linear.predict(x_test)
 #prints the predictions
 for x in range(len(predictions)):
     print(predictions[x], x_test[x], y_test[x])
+
+p = "G1"
+style.use("ggplot")
+pyplot.scatter(data[p], data["G3"])
+pyplot.xlabel(p)
+pyplot.ylabel("Final grade")
+pyplot.show()
