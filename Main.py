f4927ff2ac4ef0bb2cb125e3d6744550ff181de0
lkri <lkri@techcollege.dk>
2020-07-02 13:54:28 +0200

    clean up code and added if statement insted of out commented training model.

--- Main.py
+++ Main.py
@@ -8,62 +8,65 @@ import matplotlib.pyplot as pyplot
 from matplotlib import style
 import pickle
 
-#diffing dataset and seperators
+# constant bool to determines whether to train new model or load from file.
+isTrainingNewModel = False
+# diffing dataset and seperators
 data = pd.read_csv("student-mat.csv", sep=";")
-#filtering dataset
+# filtering dataset
 data = data[["G1", "G2", "G3", "studytime", "absences"]]
 
-#Which attribute to be predicted
+# Which attribute to be predicted
 predict = "G3"
 
-#Drops the value in predict from dataset
+# Drops the value in predict from dataset
 x = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
-#Splits dataset up into trainig data and test data, so it does not train on the same data as it tests on.
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 
-"""
-#for loob code is for retraining a new model and saving it to a pickle file if accuracy is better
-bestAcc = 0
-for _ in range(500):
-    #Splits dataset up into trainig data and test data, so it does not train on the same data as it tests on.
+if isTrainingNewModel:
+    #for loob code is for retraining a new model and saving it to a pickle file if accuracy is better
+    bestAcc = 0
+    for _ in range(500):
+        #Splits dataset up into trainig data and test data, so it does not train on the same data as it tests on.
+        x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+
+        # makes a liniar regression model
+        linear = linear_model.LinearRegression()
+        # trains model
+        linear.fit(x_train, y_train)
+
+        # gets the accuracy of the model
+        acc = linear.score(x_test, y_test)
+        print("Accuracy: \n", acc)
+
+        if acc > bestAcc:
+            bestAcc = acc
+
+            # saves the model in a pickel file
+            with open("studentmodel.pickle", "wb") as file:
+                pickle.dump(linear, file)
+
+    print("Best Accuracy: \n", bestAcc)
+else:
+    # Splits dataset up into training data and test data, so it does not train on the same data as it tests on.
     x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 
-    # makes a liniar regression model
-    linear = linear_model.LinearRegression()
-    # trains model
-    linear.fit(x_train, y_train)
-
-    # gets the accuracy of the model
-    acc = linear.score(x_test, y_test)
-    print("Accuracy: \n", acc)
-
-    if acc > bestAcc:
-        bestAcc = acc
-
-        # saves the model in a pickel file
-        with open("studentmodel.pickle", "wb") as f:
-            pickle.dump(linear, f)
-
-print("Best Accuracy: \n", bestAcc)"""
-
-#loads the trained model from a pickle file
-pickle_in = open("studentmodel.pickle", "rb")
-linear = pickle.load(pickle_in)
+    # loads the trained model from a pickle file
+    pickle_in = open("studentmodel.pickle", "rb")
+    linear = pickle.load(pickle_in)
 
 # gets the accuracy of the model
 acc = linear.score(x_test, y_test)
 print("Accuracy: \n", acc)
 
-#liniar regrssion function: Y = m + n + ... + z * X + b
-#gets the coefficient and the intercept values
+# liniar regrssion function: Y = m + n + ... + z * X + b
+# gets the coefficient and the intercept values
 print('Coefficient: \n', linear.coef_)
 print('Intercept: \n', linear.intercept_)
 
-#the model makes predictions for the dataset with liniar regression
+# the model makes predictions for the dataset with linear regression
 predictions = linear.predict(x_test)
 
-#prints the predictions
+# prints the predictions
 for x in range(len(predictions)):
     print(predictions[x], x_test[x], y_test[x])
 
